8c8347210d8cc61f081350903c09750b070777da
Derrick1908 <Derrick1908>
2021-08-25 11:35:53 +0530

    Added Store Manager Controller along with Html Helper Class and the Index/Edit Html Views

---
+++ MusicStoreMVC/Controllers/StoreManagerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreMVC.Models;
+using MusicStoreMVC.ViewModels;
+
+namespace MusicStoreMVC.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        
+        // GET: StoreManager/
+        public ActionResult Index()
+        {
+            var albums = storeDB.Albums
+                .Include("Genre").Include("Artist")
+                .ToList();
+
+            return View(storeDB.Albums);
+        }
+
+      
+        // GET: StoreManager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StoreManager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var viewModel = new StoreManagerViewModel
+            {
+                Album = storeDB.Albums.Single(a => a.AlbumId == id),
+                Genres = storeDB.Genres.ToList(),
+                Artists = storeDB.Artists.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: StoreManager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StoreManager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MusicStoreMVC/ViewModels/StoreManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MusicStoreMVC.Models;
+
+namespace MusicStoreMVC.ViewModels
+{
+    public class StoreManagerViewModel
+    {
+        public Album Album { get; set; }
+        public List<Artist> Artists { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
